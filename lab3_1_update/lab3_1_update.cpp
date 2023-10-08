e7e89c47b5ed6a7b10c9dc9080ffeca69ea312df
Newstar12321 <tazeev.zhenya@mail.ru>
2023-10-08 22:33:23 +0700

    Добавьте файлы проекта.

---
+++ lab3_1_update/lab3_1_update.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int num;
+	cout << "Введите ваше число ";
+	cin >> num;
+	if ((num < 100) || (num >= 1000))
+		{
+		cerr << "Ошибка: число должно быть положительным трехзначным" << endl;
+		return 1;
+		}
+	if (
+		((num / 100) != (num % 100 / 10)) &&
+		((num / 100) != (num % 10)) &&
+		((num % 100 / 10) != (num % 10))
+		)
+	{
+		cout << "Все цифры числа различны" << endl;
+	}
+	else
+	{
+		cout << "Не все цифры числа различны" << endl;
+	}
+}
